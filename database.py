29c968a42c638f7a7fa2ea7bfdc7ab10312ef463
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 23:22:10 -0500

    Update database connection method to use get_connection
    Refactor database connection to use environment variables for PostgreSQL connection parameters.

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
-from databricks_lakebase import Connection
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
-connection = Connection()
+def get_connection():
+    import psycopg2
+
+    return psycopg2.connect(
+        host=os.environ["PGHOST"],
+        port=os.environ["PGPORT"],
+        database=os.environ["PGDATABASE"],
+        user=os.environ["PGUSER"],
+        sslmode="require"
+    )
 
 engine = create_engine(
     "postgresql+psycopg2://",
-    creator=connection.get_connection
+    creator=get_connection,
+    poolclass=NullPool
 )
