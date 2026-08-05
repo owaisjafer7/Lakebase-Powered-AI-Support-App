73ee36dfa635a4e8cb33836785897fd72f76ac31
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 22:57:57 -0500

    Refactor database connection setup
    Removed Databricks client and credential generation for database connection.

--- database.py
+++ database.py
@@ -1,16 +1,8 @@
 import os
 from sqlalchemy import create_engine
-from databricks.sdk import WorkspaceClient
-
-client = WorkspaceClient()
-
-credential = client.database.generate_database_credential(
-    endpoint=os.environ["PGHOST"]
-)
 
 DATABASE_URL = (
-    f"postgresql://{os.environ['PGUSER']}:"
-    f"{credential.token}@"
+    f"postgresql://{os.environ['PGUSER']}@"
     f"{os.environ['PGHOST']}:"
     f"{os.environ['PGPORT']}/"
     f"{os.environ['PGDATABASE']}?sslmode=require"
