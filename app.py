126b1c657ed613d023d32e673910c7defe5e3a2b
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 22:53:25 -0500

    Update database connection to use Databricks SDK
    Replaced environment variables with Databricks credentials for database connection.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-import os
+from databricks.sdk import WorkspaceClient
 from sqlalchemy import create_engine
+import os
+
+w = WorkspaceClient()
+
+connection = w.database.get_database_credential(
+    database_instance_name="databricks_postgres"
+)
 
 DATABASE_URL = (
-    f"postgresql://{os.environ['PGUSER']}:"
-    f"{os.environ['PGPASSWORD']}@"
+    f"postgresql://{connection.username}:"
+    f"{connection.password}@"
     f"{os.environ['PGHOST']}:"
     f"{os.environ['PGPORT']}/"
     f"{os.environ['PGDATABASE']}"
