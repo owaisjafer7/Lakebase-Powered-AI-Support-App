542254636d9902ddb7eaa9763fda1079e9143b3a
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 23:33:48 -0500

    Create show_environment function for environment keys
    Refactor environment check into a function.

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 import os
-import streamlit as st
 
-st.title("Environment Check")
-
-for key in sorted(os.environ.keys()):
-    if "PG" in key or "DB" in key or "DATABASE" in key:
-        st.write(key)
-
-raise Exception("stop")
+def show_environment():
+    return [
+        key
+        for key in sorted(os.environ.keys())
+        if "PG" in key or "DB" in key or "DATABASE" in key
+    ]
