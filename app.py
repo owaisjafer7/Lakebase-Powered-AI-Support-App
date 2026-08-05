7fc371d0fbf53fd8aa99495f99c8c6ddc4380214
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 22:52:00 -0500

    Refactor DATABASE_URL construction for PostgreSQL

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.environ["database"]
+DATABASE_URL = (
+    f"postgresql://{os.environ['PGUSER']}:"
+    f"{os.environ['PGPASSWORD']}@"
+    f"{os.environ['PGHOST']}:"
+    f"{os.environ['PGPORT']}/"
+    f"{os.environ['PGDATABASE']}"
+)
 
 engine = create_engine(DATABASE_URL)
