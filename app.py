3b43e9809041952b5bb66d6a215864244a9ccf13
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 23:54:40 -0500

    Clean up app.py by removing unused code
    Removed unused imports and data fetching from database.

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import streamlit as st
-import pandas as pd
-from sqlalchemy import text
 from database import engine
-
 
 st.title("Support Ticket System")
 
-
 try:
     with engine.connect() as conn:
-        df = pd.read_sql(
-            text("SELECT * FROM tickets LIMIT 10"),
-            conn
-        )
-
-    st.success("Connected to Lakebase!")
-    st.dataframe(df)
+        st.success("Connected to Lakebase!")
 
 except Exception as e:
     st.error("Could not connect to Lakebase")
