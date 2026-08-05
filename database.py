f6f45ffeeddc252e8400dce8e1f59c16c12e6d04
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 23:37:29 -0500

    Update database connection method to use psycopg2
    Refactor database connection to use psycopg2 for SSL support.

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
 import os
+import psycopg2
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
-DATABASE_URL = (
-    f"postgresql://{os.environ['PGUSER']}@"
-    f"{os.environ['PGHOST']}:"
-    f"{os.environ['PGPORT']}/"
-    f"{os.environ['PGDATABASE']}?sslmode=require"
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.environ["PGHOST"],
+        port=os.environ["PGPORT"],
+        database=os.environ["PGDATABASE"],
+        user=os.environ["PGUSER"],
+        sslmode=os.environ["PGSSLMODE"],
+    )
+
+
+engine = create_engine(
+    "postgresql+psycopg2://",
+    creator=get_connection,
+    poolclass=NullPool
 )
-
-engine = create_engine(DATABASE_URL)
