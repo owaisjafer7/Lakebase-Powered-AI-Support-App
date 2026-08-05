1de62704aa7fc982fd0e5d57ae7497fa86b4bc7f
Owais Jafer <94933038+owaisjafer7@users.noreply.github.com>
2026-08-04 23:16:55 -0500

    Update DATABASE_URL for PostgreSQL connection

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.environ["LAKEBASE_URL"]
+DATABASE_URL = (
+    f"postgresql://{os.environ['PGUSER']}@"
+    f"{os.environ['PGHOST']}:"
+    f"{os.environ['PGPORT']}/"
+    f"{os.environ['PGDATABASE']}?sslmode=require"
+)
 
 engine = create_engine(DATABASE_URL)
